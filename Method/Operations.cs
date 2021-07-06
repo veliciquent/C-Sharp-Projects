c63654ac159437a98937c566a07f85ff4bf415fb
Joshua Ramos <veliciquent@gmail.com>
2021-07-06 04:41:14 -0600

    Add Method submission

---
+++ Method/Operations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Operations
+    {
+        public static int Power(int input, int power = 2)
+        {
+            if (power == 0)
+            {
+                return 1;
+            }
+            int tempInput = input;
+            for (int i = 1; i < power; i++)
+            {
+                tempInput = tempInput * input;
+            }
+            return tempInput;
+        }
+    }
+}
---
+++ Method/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for their selected integer to operate on
+            Console.WriteLine("What number would you like to operate on?");
+            int input = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("What number would you like to take it to the power of? If left blank it will square the number by default");
+            try
+            {
+                int power = Int32.Parse(Console.ReadLine());
+                Console.WriteLine(Operations.Power(input, power));
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(Operations.Power(input));
+            }
+            // Calling the "Operations" class and returning the value to the console
+            Console.ReadLine();
+        }
+    }
+}
