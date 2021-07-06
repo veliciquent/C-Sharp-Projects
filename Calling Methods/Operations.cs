69485ebaaffe2ffde4c619154c07b14a9ed018ab
Joshua Ramos <veliciquent@gmail.com>
2021-07-06 04:03:34 -0600

    Add Calling Methods submission

---
+++ Calling Methods/Operations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    class Operations
+    {
+        //Creating 3 methods for the "Operations" class, that each mathemtically operate on the given input
+        public static int Square(int input)
+        {
+            return input * input;
+        }
+
+        public static float Inverse(int input)
+        {
+            return 1 / (float)input;
+        }
+
+        public static float Percent (int input)
+        {
+            return (float)input * 100;
+        }
+    }
+}
---
+++ Calling Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for their selected integer to operate on
+            Console.WriteLine("What number would you like to operate on?");
+            int input = Int32.Parse(Console.ReadLine());
+            // Calling the "Operations" class for each method and returning the result to the console
+            Console.WriteLine("Square: " + Operations.Square(input) + "\r\nInverse: " + Operations.Inverse(input) + "\r\nPercentage: " + Operations.Percent(input) + "%");
+            Console.ReadLine();
+        }
+    }
+}
