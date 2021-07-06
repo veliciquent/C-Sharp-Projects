1b06ef257a77eb119df3104e50ec21a1dca51612
Joshua Ramos <veliciquent@gmail.com>
2021-07-06 04:51:29 -0600

    Add Method Class submission

---
+++ Method Class/Operations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    class Operations
+    {
+        // Creating a method that multiplies the given number by a power, that is an overloaded argument
+        public static void notRandom(int input, int display)
+        {
+            input = input * display;
+            Console.WriteLine(display);
+        }
+    }
+}
---
+++ Method Class/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calls the method with defined arguments for code legibility
+            Operations.notRandom(input: 2, display: 12);
+            Console.ReadLine();
+        }
+    }
+}
