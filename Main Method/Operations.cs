5c642ce13bddfb6f78461ced9299a1afcbca5c81
Joshua Ramos <veliciquent@gmail.com>
2021-07-06 04:20:32 -0600

    Add Main Method submission

---
+++ Main Method/Operations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class Operations
+    {
+        //Creating 3 methods for the "Operations" class, that each mathemtically operate on the given input based on the input type
+        public static int notRandom(int input)
+        {
+            return input * input;
+        }
+
+        public static int notRandom(float input)
+        {
+            return (int)(input / 5);
+        }
+
+        public static int notRandom(string input)
+        {
+            return Int32.Parse(input) * 100;
+        }
+    }
+}
---
+++ Main Method/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for their selected integer to operate on
+            Console.WriteLine("What number would you like to operate on?");
+            int input = Int32.Parse(Console.ReadLine());
+            // Calling the "Operations" class for each method and casting the input as the different input types, returning the result to the console
+            Console.WriteLine("Int: " + Operations.notRandom(input) + "\r\nFloat: " + Operations.notRandom((float)input) + "\r\nString: " + Operations.notRandom(input.ToString()));
+            Console.ReadLine();
+        }
+    }
+}
