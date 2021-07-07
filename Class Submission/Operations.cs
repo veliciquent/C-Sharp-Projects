df67f3f34cf2383cb9afa20c4e74e70305af1cf3
Joshua Ramos <veliciquent@gmail.com>
2021-07-07 01:05:05 -0600

    Add Class Submission

---
+++ Class Submission/Operations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Submission
+{
+    class Operations
+    {
+        // Creating a class with an output and an overloaded input that halves the given input
+        public static void Halve(out int output, int input = 2)
+        {
+            output = input / 2;
+        }
+    }
+}
---
+++ Class Submission/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating the class
+            Operations Operations = new Operations();
+            // Requesting the user input for the number to halve
+            Console.WriteLine("Enter a number to halve");
+            int input = Int32.Parse(Console.ReadLine());
+            // Calling the operation on the user input and returning it to the console
+            Operations.Halve(out input, input);
+            Console.WriteLine("Your halved number is; " + input);
+            Console.ReadLine();
+        }
+    }
+}
