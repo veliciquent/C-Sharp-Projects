8b23884f066d68dbb7edbfdfa27516f276be3676
Joshua Ramos <veliciquent@gmail.com>
2021-07-07 04:31:31 -0600

    Add Parameters submission

---
+++ Parameters/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters
+{
+    class Employee<T> : Person
+    {
+        // Creating a sublcass of person that has a generic that is included as a new list of "things"
+        public List<T> things = new List<T>();
+        public override void SayName()
+        {
+            base.SayName();
+        }
+
+        // Creating a method that when called iterates through the list of "things"
+        public void SayThings()
+        {
+            foreach ( T thing in things)
+            {
+                Console.WriteLine(thing);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Parameters/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating two employees with different passed variable types.
+            Employee<string> employee = new Employee<string> { things = new List<string> { "Winner", "winner", "chicken", "dinner" } };
+            Employee<int> employee2 = new Employee<int> { things = new List<int> { 3214, 254 ,48, 435 } };
+            employee.SayThings();
+            employee2.SayThings();
+        }
+    }
+}
