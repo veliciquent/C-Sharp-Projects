0afd249e50df2c9139a14293945a93a2511874bb
Joshua Ramos <veliciquent@gmail.com>
2021-07-07 01:41:56 -0600

    Add Methods and Objects Submission

---
+++ Methods and Objects/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Objects
+{
+    public class Person
+    {
+        // Creating Superclass to the Employee class with given properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Methods and Objects/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating inheriting class with given values
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student", Id = 3245 };
+            // Calling the "Say Name" function from the superclass
+            employee.SayName();
+        }
+    }
+}
