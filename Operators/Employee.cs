bc7069ae788ff4bcae24c9fb0eef83052da04a8c
Joshua Ramos <veliciquent@gmail.com>
2021-07-07 04:08:32 -0600

    Add Operators submission

---
+++ Operators/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Employee : Person
+    {
+        public int Id { get; set; }
+        // Creating a subclass from the "Person" Class and overriding the "==" and "!=" operators to return an boolean in relevance to the employee ID
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            return employee1.Id == employee2.Id;
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return employee1.Id != employee2.Id;
+        }
+    }
+}
---
+++ Operators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating two employees with varying ID's
+            Employee employee1 = new Employee() { FirstName = "Jim", LastName = "Browning", Id = 32459 };
+            Employee employee2 = new Employee() { FirstName = "Imbiamba", LastName = "Jones", Id = 34547 };
+            // Using the overloaded "=" operator to compare employee ID's
+            Console.WriteLine("Is the ID of employee 1 equal to employee 2? \r\n" + (employee1 == employee2));
+            Console.ReadLine();
+        }
+    }
+}
