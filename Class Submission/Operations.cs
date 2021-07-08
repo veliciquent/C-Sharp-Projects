189fa8d3daaa79ec2189d7c4652398785abdeb76
Joshua Ramos <veliciquent@gmail.com>
2021-07-08 00:05:37 -0600

    Fix Class static method

--- Class Submission/Operations.cs
+++ Class Submission/Operations.cs
@@ -9,7 +9,7 @@ namespace Class_Submission
     class Operations
     {
         // Creating a class with an output and an overloaded input that halves the given input
-        public static void Halve(out int output, int input = 2)
+        public void Halve(out int output, int input = 2)
         {
             output = input / 2;
         }
