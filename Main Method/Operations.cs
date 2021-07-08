a4d4d3a71b7af879182d403db5a6dcfcbc93dbd9
Joshua Ramos <veliciquent@gmail.com>
2021-07-08 00:08:28 -0600

    Fix Main Method static method

--- Main Method/Operations.cs
+++ Main Method/Operations.cs
@@ -9,17 +9,17 @@ namespace Main_Method
     class Operations
     {
         //Creating 3 methods for the "Operations" class, that each mathemtically operate on the given input based on the input type
-        public static int notRandom(int input)
+        public int notRandom(int input)
         {
             return input * input;
         }
 
-        public static int notRandom(float input)
+        public int notRandom(float input)
         {
             return (int)(input / 5);
         }
 
-        public static int notRandom(string input)
+        public int notRandom(string input)
         {
             return Int32.Parse(input) * 100;
         }
