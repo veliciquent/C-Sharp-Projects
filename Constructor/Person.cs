4c61010b93f70a645da0effc053a600d3458f259
Joshua Ramos <veliciquent@gmail.com>
2021-07-08 02:10:37 -0600

    Add Constructor submission

---
+++ Constructor/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Person
+    {
+        // Creating initial constructor
+        public Person(string FirstName, string LastName, DateTime Birthday)
+        {
+            firstName = FirstName;
+            lastName = LastName;
+            birthday = Birthday;
+        }
+        // Creating chained constructor
+        public Person(string FirstName) : this(FirstName, "", DateTime.Now)
+        {
+
+        }
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public DateTime birthday { get; set; }
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an abritrary constant string
+            const string nope = "I AM ETERNAL";
+            // Creating an implied variable utilizing "var"
+            var implied = Guid.NewGuid();
+        }
+    }
+}
