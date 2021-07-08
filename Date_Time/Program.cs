be15cf0f55b86702c9aba4065ffbcc1cca9b2839
Joshua Ramos <veliciquent@gmail.com>
2021-07-08 01:57:25 -0600

    Update Date Time with comments

--- Date_Time/Program.cs
+++ Date_Time/Program.cs
@@ -10,9 +10,12 @@ namespace Date_Time
     {
         static void Main(string[] args)
         {
+            // Tell user current time
             Console.WriteLine("The current time is: {0}", DateTime.Now);
+            // Prompt user for a number, then store that number as a timespan
             Console.WriteLine("Please enter a number of hours");
             TimeSpan hours = new TimeSpan(0, Int32.Parse(Console.ReadLine()), 0 ,0);
+            // Combine the current date/time with user timespan
             Console.WriteLine("The new time {0} hours from now is {1}", hours.Hours, DateTime.Now + hours);
             Console.ReadLine();
         }
