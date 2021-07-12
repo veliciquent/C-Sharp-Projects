5d936070c99bf8f089d2ad6392b4aa976f8df658
Joshua Ramos <veliciquent@gmail.com>
2021-07-12 08:44:02 -0600

    Add Car Insurance Submission

---
+++ CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarInsurance.Models;
+using CarInsurance.ViewModels;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        private InsuranceEntities2 db = new InsuranceEntities2();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(db.Insurees.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CarInsurance/Models/Metadata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using CarInsurance.Models;
+
+
+namespace CarInsurance.ViewModels
+{
+    public class InsureeVm
+    {
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [StringLength(50)]
+        public string LastName { get; set; }
+        [StringLength(50)]
+        public string EmailAddress { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public System.DateTime DateOfBirth { get; set; }
+        public int CarYear { get; set; }
+        [StringLength(50)]
+        public string CarMake { get; set; }
+        [StringLength(50)]
+        public string CarModel { get; set; }
+        public bool DUI { get; set; }
+        public int SpeedingTickets { get; set; }
+        public bool CoverageType { get; set; }
+        public decimal Quote { get; set; }
+    }
+}
